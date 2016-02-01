ceaf5c7a2619cb0e77dcea308708b869a74d1bb8
schellap <schellap@microsoft.com>
2016-01-31 21:20:30 -0800

    publish to dir

--- infra/scripts/build.ps1
+++ infra/scripts/build.ps1
@@ -3,6 +3,6 @@
 If (-Not (Test-Path "$Packaging")) {
     Invoke-Expression "$DotNet restore `"$ProjectRoot\infra\packaging`" -s https://www.myget.org/F/dotnet-core/ -s https://www.myget.org/F/dotnet-corefxlab/ -s https://www.nuget.org/api/v2/"
     If ($LastExitCode -ne 0) { Exit 1 }
-    Invoke-Expression "$DotNet build `"$ProjectRoot\infra\packaging`" -c Release -o `"$BinRoot\infra\packaging`""
+    Invoke-Expression "$DotNet publish `"$ProjectRoot\infra\packaging`" -c Release -o `"$BinRoot\infra\packaging`""
     If ($LastExitCode -ne 0) { Exit 1 }
 }
--- infra/scripts/build.sh
+++ infra/scripts/build.sh
@@ -1,7 +1,7 @@
 __script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source ${__script_dir}/common.sh
 
-if [ ! -f "${__Pacakaging}" ]; then
+if [ ! -f "${__Packaging}" ]; then
     ${__DotNet} restore ${__ProjectRoot}/infra/packaging -s https://www.myget.org/F/dotnet-core/ -s https://www.myget.org/F/dotnet-corefxlab/ -s https://www.nuget.org/api/v2/
-    ${__DotNet} build ${__ProjectRoot}/infra/packaging -c Release -o ${__BinRoot}/infra/packaging
+    ${__DotNet} publish ${__ProjectRoot}/infra/packaging -c Release -o ${__BinRoot}/infra/packaging
 fi
